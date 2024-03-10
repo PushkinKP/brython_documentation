647ef1d8aa3d0913138384ed23790bea62510441
Jules Cnockaert <cnockaertjules@gmail.com>
2024-03-10 23:34:39 +0100

    Update collecte_brython_pages_cachees.py

--- collecte/collecte_brython_pages_cachees.py
+++ collecte/collecte_brython_pages_cachees.py
@@ -10,6 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+import bs4
 from time import sleep
 import pandas as pd 
 
@@ -21,66 +22,60 @@ driver.get("https://brython.info/index.html")
 
 sleep(2)
 
-# A refaire en utilisant les selecteurs CSS, en faisant un "tableau"/dictionnaire pour les pages(redemander à Denis) 
-# Pages cachées = liens dans les pages qui ne sont pas visibles(c-à-d pas dans les selecteurs)
-#   |   |
-#   |   |
-#   v   v
-
-links = []
-len_links = []
-for i in range(1,7): 
-    page = driver.find_element(by = By.XPATH, value = f'/html/body/div[1]/a[{i}]')
-    page.click()
-    sleep(3) 
-    if (i == 1):
-        sleep(1)
-    if (i == 2):
-        links2 = driver.find_elements(by = By.XPATH, value = '/html/body/div[3]/div/div[1]/ul/li')
-        res2 = links.append(links2)
-        len_links.append(len(links2))
-        sleep(1)
-    elif (i == 3):
-        links3 = driver.find_elements(by = By.XPATH, value = '/html/body/table/tbody/tr/td[1]/h4' and '/html/body/table/tbody/tr/td[1]/div/a')
-        res3 = links.append(links3)
-        len_links.append(len(links3))
-        sleep(1)
-    elif (i == 4):
-        sleep(1)
-    elif (i == 5):
-        sleep(1)
-    elif (i == 6): # ne fonctionne pas
-        links6 = driver.find_elements(by = By.XPATH, value = '/html/body/div[3]/table/tbody/tr/td[1]/a' and '/html/body/div[3]/table/tbody/tr/td[2]/a')
-        res6 = links.append(links6)
-        len_links.append(len(links6))
-        sleep(1)
-
-links7 = driver.find_elements(by = By.XPATH, value = '/html/body/div[1]/table/tr[1]/td') # ne fonctionne pas
-res7 = links.append(links7)
-len_links.append(len(links7))
-sleep(1)
-
-print(links, len_links)
-        
-# Il y a 7 pages mais on se concentre seulement sur les 3 premières et les 2 dernières qui sont les plus intéressantes
-# Dans la page 1 'Tutoriel' il y a des selecteurs de mais c'est toujours la meme page donc on ne les comptera pas
-# Dans la page 2 'Démo' les selecteurs de pages sont en liste alors que dans la page 3 'Documentation' les selecteurs de pages sont en tableau (possible correction à apporter)
-# La page 7 'Ressources' n'est pas une page à part entière, on ne peut donc pas l'inclure dans la boucle
- 
-
- # Test
-
 links_pages = {
-    "Tutoriel": "banner_row > a:nth-child(2)", 
-    "Demo": "banner_row > a:nth-child(3)", 
-    "Documentation": "banner_row > a:nth-child(4)", 
-    "Console": "banner_row > a:nth-child(5)",
-    "Editeur": "banner_row > a:nth-child(6)",
-    "Galerie": "banner_row > a:nth-child(7)"}
+    "Tutoriel": "#banner_row > a:nth-child(2)",
+    "Demo": "#banner_row > a:nth-child(3)", 
+    "Documentation": "#banner_row > a:nth-child(4)", 
+    "Console": "#banner_row > a:nth-child(5)",
+    "Editeur": "#banner_row > a:nth-child(6)",
+    "Galerie": "#banner_row > a:nth-child(7)"
+}
 
-for i in links_pages:
-    page = driver.find_element(By.CSS_SELECTOR, links_pages[i]).click()
-    sleep(3)
+hidden_links_all_pages = {}
+count_all = 0
+count_real_hidden = 0
 
+for page_name, css_selector in links_pages.items():
+    driver.find_element(By.CSS_SELECTOR, css_selector).click()
+    sleep(2)
+    url_page = driver.current_url
     
-sleep(10)
+    soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+
+    all_links = soup.find_all('a')
+
+    hidden_links = []
+    real_hidden_links = []
+    for link in all_links:
+        href = link.get('href')
+        if href:
+            is_hidden = all(css_selector not in href for css_selector in links_pages.values())
+            if is_hidden:
+                hidden_links.append(href)
+                count_all += 1
+                if not href.startswith("https") and href not in ('/index.html', '/static_tutorial/fr/index.html', '/demo.html?lang=fr', '/static_doc/3.12/fr/intro.html', '/tests/console.html?lang=fr', '/tests/editor.html?lang=fr', '/gallery/gallery_fr.html'):
+                    real_hidden_links.append(href)
+                    count_real_hidden += 1
+
+    hidden_links_all_pages[page_name] = hidden_links
+
+print("Liens cachés pour toutes les pages :")
+for page_name, hidden_links in hidden_links_all_pages.items():
+    print("\n---------------------\n")
+    print("Page :", page_name, "\nUrl : ", url_page, "\n")
+    if hidden_links:
+        for hidden_link in hidden_links:
+            if hidden_link in real_hidden_links:
+                print("Page cachée :", hidden_link)
+            else:
+                print("Page externe :", hidden_link)
+    else:
+        print("Aucun lien caché trouvé sur cette page.")
+
+print("\n\nCompte total de pages cachées : ", count_all, "\nCompte de vrai pages cachées : ", count_real_hidden)
+print("\n\nVrais liens cachées : \n", real_hidden_links)
+
+driver.quit()
+
+# On se concentre uniquement sur les pages du sites cachées, les pages externe type GitHub ne seront pas comptabilisé, les vraies pages cachées sont les pages de Brython qui ne sont pas référencer directement (c'est-à-dire par des liens).
+# On ne comptabilise pas non plus ('/index.html', '/static_tutorial/fr/index.html', '/demo.html?lang=fr', '/static_doc/3.12/fr/intro.html', '/tests/console.html?lang=fr', '/tests/editor.html?lang=fr', '/gallery/gallery_fr.html') qui sont les pages principales du sites et donc par définition pas cachées
