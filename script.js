eda9f4d5e504ef3687cbc1cf60f8fcdcf5a55922
Jules Cnockaert <cnockaertjules@gmail.com>
2024-03-28 02:24:57 +0100

    Added Dark Mode

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
-// Références vers les éléments HTML
-const consoleInput = document.getElementById('console-input');
-const consoleRun = document.getElementById('console-run');
-const consoleOutput = document.getElementById('console-output');
+const btnToggle = document.querySelector('.btn-toggle');
 
-// Fonction pour exécuter la commande saisie dans la console
-function executeCommand() {
-  try {
-    // Récupère la commande saisie par l'utilisateur
-    const command = consoleInput.value.trim();
+btnToggle.addEventListener('click', () => {
 
-    // Vérifie si la commande est vide
-    if (command === '') {
-      throw new Error('Veuillez saisir une commande.');
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+        btnToggle.innerHTML = ""
+
+    } else if(body.classList.contains('light')){
+
+        body.classList.add('dark')
+        body.classList.remove('light')
+        btnToggle.innerHTML = ""
+
     }
 
-    // Évalue la commande de manière sécurisée avec Function() plutôt que eval()
-    // Cela évite l'exécution de code potentiellement dangereux
-    const result = Function('"use strict";return (' + command + ')')();
-
-    // Affiche le résultat dans la console
-    consoleOutput.textContent = result;
-  } catch (error) {
-    // Affiche l'erreur dans la console
-    consoleOutput.textContent = error.message;
-  }
-}
-
-// Écouteur d'événement pour le bouton "Exécuter"
-consoleRun.addEventListener('click', executeCommand);
-
-
-function updateClock() {
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  document.getElementById('clock').innerText = `${hours}:${minutes}:${seconds}`;
-}
-setInterval(updateClock, 1000);
-updateClock();
-
-
+})
